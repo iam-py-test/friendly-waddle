51ce27300748ff15d9700fc0537e902b47e0f772
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-26 14:05:05 -0400

    Create entry.js

---
+++ docs/entry.js
@@ -0,0 +1,9 @@
+var main = async function(){
+  var req = await fetch("blog.json")
+  var entries = JSON.parse(await req.text())
+  var id = new URL(location).searchParams.get('id') + 0
+  var cEntry = entries[id]
+  document.title = cEntry.title + " &middot; friendly-waddle"
+  document.getElementById('title').textContent = cEntry.title
+  document.getElementById('body').innerText = cEntry.body
+}
