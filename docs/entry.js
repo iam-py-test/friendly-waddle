6103d0841cc005b68a9d8ee36be9ea1b55e947e8
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-26 14:06:56 -0400

    Update entry.js

--- docs/entry.js
+++ docs/entry.js
@@ -2,6 +2,7 @@ var main = async function(){
   var req = await fetch("blog.json")
   var entries = JSON.parse(await req.text())
   var id = new URL(location).searchParams.get('id') + 0
+  console.log(id,entries,entries[id])
   var cEntry = entries[id]
   document.title = cEntry.title + " &middot; friendly-waddle"
   document.getElementById('title').textContent = cEntry.title
