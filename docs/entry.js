7f2c213a89bb8c10b7d51dda932faaf8754f47ff
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-26 15:22:22 -0400

    Support autolinking

--- docs/entry.js
+++ docs/entry.js
@@ -1,3 +1,23 @@
+
+//from https://stackoverflow.com/a/2166104
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
+
 var main = async function(){
   var req = await fetch("blog.json")
   var entries = JSON.parse(await req.text())
@@ -11,7 +31,10 @@ var main = async function(){
   }
   document.title = cEntry.title + " · friendly-waddle"
   document.getElementById('title').textContent = cEntry.title
-  document.getElementById('body').innerText = cEntry.body
+  var encode = document.createElement('span')
+  encode.innerText = cEntry.body
+  var linkedbody = linkify(encode.innerHTML)
+  document.getElementById('body').innerHTML = linkedbody
 }
 
 main()
