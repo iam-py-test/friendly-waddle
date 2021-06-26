315acc517dc44023be8d92e996ff079023f97660
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-26 14:14:48 -0400

    Update entry.js

--- docs/entry.js
+++ docs/entry.js
@@ -4,6 +4,11 @@ var main = async function(){
   var id = new URL(location).searchParams.get('id') * 1
   console.log(id,entries,entries[id])
   var cEntry = entries[id]
+  if(cEntry === undefined || cEntry === null){
+    document.title = "Entry not found"
+    document.getElementById('title').textContent = 'Entry not found'
+    return;
+  }
   document.title = cEntry.title + " · friendly-waddle"
   document.getElementById('title').textContent = cEntry.title
   document.getElementById('body').innerText = cEntry.body
